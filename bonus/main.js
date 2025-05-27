0eaba64f039566a2e8cb9c75ea2c805248b0ba37
Edoardo Lova <edoardolova81@gmail.com>
2025-05-27 17:47:23 +0200

    add pari o dispari controls

--- bonus/main.js
+++ bonus/main.js
@@ -4,16 +4,25 @@ const userChoice = prompt("pari o dispari?");
 const userNumber = Number(prompt("scegli un numero compreso tra 1 e 9"));
 const pcNumber = Math.floor(Math.random() * 9) + 1;
 
-console.log("userNumber:" + userNumber);
-console.log(`pcNumber: ${pcNumber}`);
-let result = userNumber + pcNumber;
 
+let result = userNumber + pcNumber;
 const isEven = result % 2 === 0;
-if (userChoice.toLowerCase() === "pari" && isEven || userChoice.toLowerCase() === "dispari" && !isEven) {
-    console.log("HAI VINTO!!!");
+
+
+// controls 
+if (userChoice.toLowerCase() !== "pari" || userChoice.toLowerCase() !== "dipari" && userNumber > 9 || userNumber < 1) {
+    console.log("Valore non valido, ricarica la pagina");
 }
 else{
-    console.log("HAI PERSO");
+    console.log("userNumber:" + userNumber);
+    console.log(`pcNumber: ${pcNumber}`);
+    if (userChoice.toLowerCase() === "pari" && isEven || userChoice.toLowerCase() === "dispari" && !isEven) {
+        console.log("HAI VINTO!!!");
+    }
+    else{
+        console.log("HAI PERSO");
+    }
+
 }
 
 
